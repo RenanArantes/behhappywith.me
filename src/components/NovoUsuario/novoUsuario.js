4a324957ff6303b82baf8149860c428725c4a191
[RenanArantes] <[renan.arantes@rede.ulbra.br]>
2019-12-25 22:24:36 -0300

    adição_de_label

--- src/components/NovoUsuario/novoUsuario.js
+++ src/components/NovoUsuario/novoUsuario.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Label from '../Label/label.js';
+
 class NovoUsuario extends React.Component {
     render() {
         return (
             <div className="center">
+                <form className="pure-form pure-form-stacked">
+                    <Label htmlFor="nome" texto="Quem é você?" />
+                </form>
             </div>
         );
     }
